ca80f710bf691dd6621f37d73ad63bf05850e305
Reuven Harrison <reuvenharrison@gmail.com>
2025-04-18 13:05:23 +0300

    enable google analytics with Google Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import type { Metadata } from "next"
+import Script from 'next/script'
 import { MainNav } from "./components/MainNav"
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,6 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
+      <head>
+        <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-C7KPYNHWS0`} />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);
+            gtag('js', new Date());
+            gtag('config', 'G-C7KPYNHWS0');
+          `}
+        </Script>
+      </head>
       <body className="bg-[var(--background)] text-[var(--foreground)] antialiased pt-16">
         <MainNav />
         <main className="">
